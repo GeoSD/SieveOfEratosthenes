2109446d99ce341abee44cf31e8aec4916ba876a
Georgy Dyagilev <g.dyagilev@gmail.com>
2019-04-15 15:21:00 +0700

    Interim commit

---
+++ SieveOfEratosthenes/Model/PrimeNumber.swift
@@ -0,0 +1,12 @@
+//
+//  PrimeNumber.swift
+//  SieveOfEratosthenes
+//
+//  Created by Georgy Dyagilev on 14/04/2019.
+//  Copyright © 2019 Georgy Dyagilev. All rights reserved.
+//
+
+import Foundation
+struct PrimeNumber {
+    let primeNumber: Int
+}
